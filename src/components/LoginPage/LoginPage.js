ad83ad7ad4e9468c7655022c7b0023bf1c809e2b
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-09 13:00:16 +0100

    Scaffold LoginPage

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from "styled-components";
+import logoIcon from '../../logo-icon.png'
+import logoText from '../../logo-text.png'
 
 
 class LoginPage extends React.Component {
     render() {
         return (
-            <div>LoginPage</div>
+            <div className='wrapper'>
+                <nav className='navigation'>
+                    <div className='LogoContainer'>
+                        <div className='LogoIcon'>
+                            <img src={logoIcon} />
+                        </div>
+                        <div className='LogoText'>
+                            <img src={logoText} />
+                        </div>
+                    </div>
+                    <ul className='links'>
+                        <li>Terms</li>
+                        <li>Privacy</li>
+                    </ul>
+                </nav>
+            </div>
         );
     }
 }
