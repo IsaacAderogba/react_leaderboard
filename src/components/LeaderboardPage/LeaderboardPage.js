5e37ba1d5629e72f981bba7840404099dc9a355f
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-10 15:29:36 +0100

    Configure charts

--- src/components/LeaderboardPage/LeaderboardPage.js
+++ src/components/LeaderboardPage/LeaderboardPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../LoginPage/Header";
 import Footer from "../LoginPage/Footer";
 
-import Chart from "./Chart";
+import ProgressChart from "./ProgressChart";
 import styled from "styled-components";
 
 class LeaderboardPage extends React.Component {
@@ -11,29 +11,44 @@ class LeaderboardPage extends React.Component {
     this.amendedUserData = [];
     this.amendedConfigData = [];
 
-    if('data' in props.userData[0]) {
+    if ("data" in props.userData[0]) {
       // do nothing
-      console.log('executing')
+      console.log("Using local storage data");
     } else {
       this.configureData();
     }
 
     this.state = {
-        appData: this.amendedUserData.length > 1 ? this.amendedUserData : props.userData,
-        configData: this.amendedUserData.length > 1 ? this.amendedConfigData : props.configData
-    }
+      appData:
+        this.amendedUserData.length > 1 ? this.amendedUserData : props.userData,
+      configData:
+        this.amendedUserData.length > 1
+          ? this.amendedConfigData
+          : props.configData
+    };
 
-    console.log(this.state.appData, this.state.configData)
+    console.log(this.state.appData, this.state.configData);
   }
 
   configureData = () => {
     this.amendedUserData = this.props.userData.map(user => {
       console.log(user);
-     return  {...user, labels: [], data: [], currentValue: 0, points: 0, progress: 0}
+      return {
+        ...user,
+        labels: [],
+        data: [],
+        currentValue: 0,
+        points: 0,
+        progress: 0
+      };
     });
 
-    this.amendedConfigData = [...this.amendedConfigData, {yAxisLabel: 'Enter Label'}, {successMetric: 'UpTheChart'}];
-  }
+    this.amendedConfigData = [
+      ...this.amendedConfigData,
+      { yAxisLabel: "Enter Label" },
+      { successMetric: "UpTheChart" }
+    ];
+  };
 
   saveData = () => {
     localStorage.clear();
@@ -44,19 +59,7 @@ class LeaderboardPage extends React.Component {
       count++;
     });
 
-    localStorage.setItem('config', JSON.stringify(this.state.configData))
-
-    // this.state.formInputs.forEach(form => {
-    //   if (form.formValue) {
-    //     let playerObject = {
-    //       playerName: form.formValue,
-    //       playerColor: form.color
-    //     };
-    //     localStorage.setItem(count, JSON.stringify(playerObject));
-    //     count++;
-    //   }
-    // });
-
+    localStorage.setItem("config", JSON.stringify(this.state.configData));
   };
 
   render() {
@@ -66,7 +69,15 @@ class LeaderboardPage extends React.Component {
       <div>
         <Header />
         <StyledChartsContainer>
-          <Chart />
+          {this.state.appData.map(user => {
+            return (
+              <ProgressChart
+                key={user.playerName}
+                userData={user}
+                configData={this.state.configData}
+              />
+            );
+          })}
         </StyledChartsContainer>
         <Footer />
       </div>
--- src/components/LeaderboardPage/ProgressChart.js
+++ src/components/LeaderboardPage/ProgressChart.js
@@ -2,31 +2,26 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import styled from "styled-components";
 
-class Charts extends React.Component {
+class ProgressChart extends React.Component {
   constructor(props) {
     super(props);
+
+    const {playerColor, data, label } = props.userData
+    console.log(props);
     this.state = {
       data: {
-        labels: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday"
-        ],
+        labels: label,
         datasets: [
           {
             fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "#61DAFB",
+            borderColor: playerColor,
             borderCapStyle: "butt",
             borderDash: [],
             borderDashOffset: 0.0,
             borderJoinStyle: "miter",
-            pointBorderColor: "#61DAFB",
+            pointBorderColor: playerColor,
             pointBackgroundColor: "#fff",
             pointBorderWidth: 1,
             pointHoverRadius: 5,
@@ -35,7 +30,7 @@ class Charts extends React.Component {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: data
           }
         ]
       }
@@ -44,16 +39,17 @@ class Charts extends React.Component {
 
   render() {
     return (
-      <StyledChart>
+      <StyledProgressChart>
         <Line
           data={this.state.data}
           width={100}
-          height={50}
+          height={70}
           options={{
             title: {
               display: true,
-              text: "Player 1", 
-              fontColor: "#E4E6E8"
+              text: this.props.userData.playerName,
+              fontColor: "#E4E6E8",
+              fontSize: '24'
             },
             maintainAspectRatio: true,
             legend: {
@@ -77,13 +73,18 @@ class Charts extends React.Component {
             }
           }}
         />
-      </StyledChart>
+      </StyledProgressChart>
     );
   }
 }
 
-const StyledChart = styled.div`
-  width: 500px;
+const StyledProgressChart = styled.div`
+  width: 45%;
+  margin: 24px 2.5%;
+
+  @media only screen and (max-width: 700px) {
+    width: 90%;
+  }
 `;
 
-export default Charts;
+export default ProgressChart;
